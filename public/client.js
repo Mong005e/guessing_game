1339cc3ecb509ef2bd7506147421a848c43672d8
Ben Lauderbaugh <benjlaud1@gmail.com>
2017-04-21 12:05:20 -0500

    loadGame function

--- public/client.js
+++ public/client.js
@@ -2,8 +2,13 @@
 $(document).ready(onReady);
 
 function onReady () {
-
+loadGame();
+// event listeners
   $('#start').on('click', startButton);
+  $('#abandon-button').on('click', abandonGame);
+  $('#restart-button').on('click', loadGame);
+  // $('#check-guesses').on('click', checkGuesses);
+
 }
 
 function startButton () {
@@ -21,3 +26,20 @@ function startButton () {
     } //  end success
   }); // end ajax
 } // end startButton
+
+function abandonGame () {
+  var confirmAnswer = confirm("Do you really want to quit?");
+  if (confirmAnswer) {
+    console.log('quitter!!!');
+    $('.game-div').empty();
+    $('.game-div').append('<h1 id="quitter" >QUITTER!!!</h1>');
+    setTimeout (function () {loadGame() ;},5000);
+
+  }
+}
+
+function loadGame (){
+  console.log('restarted');
+  $('.game-div').empty();
+  $('.game-div').append('<p>Player 1 </p><input type="text" id="user-guess-1" placeholder="enter your guess"><p>Player 2 </p><input type="text" id="user-guess-2" placeholder="enter your guess"><p>Player 3 </p><input type="text" id="user-guess-3" placeholder="enter your guess"><p>Player 4 </p><input type="text" id="user-guess-4" placeholder="enter your guess">');
+}
