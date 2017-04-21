6b0d7689f93ede8df1e1c995dc18b58eac8c35e5
Andrew Hastings <droo.hastings@gmail.com>
2017-04-21 15:59:23 -0500

    Results Object Sending

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var numberGen = require ('./number-module');
 
 //globals
 var maxValue;
+var randomNumber = 0;
 
 //uses
 app.use ( express.static ( 'public' ) ); // if index.html is not in a sub folder, you do not need the app.get('/')
@@ -30,14 +31,47 @@ app.get ('/', function (req, res){
 app.post('/startGame', function ( req, res ){
   maxValue = parseInt(req.body.max);
   console.log('received from client:', maxValue);
-  console.log('random number generated: ' + numberGen(1, maxValue));
+
+  randomNumber = numberGen(1, maxValue);
+  randomNumber = parseInt(randomNumber);
+  console.log('random number generated: ' + randomNumber);
+  res.sendStatus(201);
 });
 
 app.post('/checkGuesses', function ( req, res ){
   var guessesObj = req.body; // req.body = objectToSend
   console.log('received from client:', guessesObj );
   //logic
+  checkGuesses(req.body.player1);
+  checkGuesses(req.body.player2);
+  checkGuesses(req.body.player3);
+  checkGuesses(req.body.player4);
 
+  var objectToSend = {
+    player1: checkGuesses(req.body.player1),
+    player2: checkGuesses(req.body.player2),
+    player3: checkGuesses(req.body.player3),
+    player4: checkGuesses(req.body.player4)
+  };
 
-  res.send('meow');
+  console.log(objectToSend);
+  res.send(objectToSend);
 });
+
+function checkGuesses (playersGuess){
+
+  playersGuess = parseInt(playersGuess);
+  // randomNumber = parseInt(randomNumber);
+  console.log('in checkGuesses player guess:', playersGuess);
+  console.log('in checkGuesses check guess', randomNumber);
+  if (randomNumber === playersGuess) {
+    return 'correct' ;
+    }
+    else if (randomNumber > playersGuess) {
+      return 'low' ;
+    }
+    else {
+      return 'high' ;
+    }
+
+}
